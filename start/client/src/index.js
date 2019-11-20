99834575bac7c7b6038d5b45760a136dec0ae053
Peter Makarov <peter.makarov@wavea-ccess.com>
2019-11-20 15:48:16 +0100

    8. Manage local state

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,11 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import gql from "graphql-tag";
-import { ApolloProvider } from '@apollo/react-hooks';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
 import Pages from './pages';
+import Login from './pages/login';
+import { resolvers, typeDefs } from './resolvers';
+import injectStyles from './styles';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -17,7 +22,9 @@ const link = new HttpLink({
 
 const client = new ApolloClient({
   cache,
-  link
+  link,
+  typeDefs,
+  resolvers,
 });
 
 cache.writeData({
@@ -42,13 +49,23 @@ client
   })
   .then(result => console.log(result));
 
+  const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
 
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
+// email to log in: daisy@apollographql.com
 
-// previous variable declarations
-
+injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
-  </ApolloProvider>, document.getElementById('root')
+    <IsLoggedIn />
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
--- start/client/src/pages/launch.js
+++ start/client/src/pages/launch.js
@@ -2,33 +2,30 @@ import React, { Fragment } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
+import { LAUNCH_TILE_DATA } from './launches';
 import { Loading, Header, LaunchDetail } from '../components';
 import { ActionButton } from '../containers';
 
 export const GET_LAUNCH_DETAILS = gql`
   query LaunchDetails($launchId: ID!) {
     launch(id: $launchId) {
-      id
+      isInCart @client
       site
-      isBooked
       rocket {
-        id
-        name
         type
       }
-      mission {
-        name
-        missionPatch
-      }
+      ...LaunchTile
     }
   }
+  ${LAUNCH_TILE_DATA}
 `;
 
 export default function Launch({ launchId }) {
   const { data, loading, error } = useQuery(
     GET_LAUNCH_DETAILS,
-    { variables: { launchId } }
+    { variables: { launchId } },
   );
+
   if (loading) return <Loading />;
   if (error) return <p>ERROR: {error.message}</p>;
 
